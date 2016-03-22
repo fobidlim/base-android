d4c539205584444f4cda22fe0816da91239af11e
Fobid <fobidbumz@naver.com>
2016-03-22 21:14:36 +0900

    first push

---
+++ library/src/main/java/com/tfc/library/ui/activity/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.tfc.library.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Fobid on 2016. 3. 22..
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
